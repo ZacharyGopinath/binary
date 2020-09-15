8fed5250be3ce9d9980baa73b6b03beb6f0913e1
ZacharyGopinath <54950453+ZacharyGopinath@users.noreply.github.com>
2020-09-15 15:10:22 -0400

    First Commit (index.html,css,js, website logo)

---
+++ index.js
@@ -0,0 +1,12 @@
+function calculate(){
+    var num = document.getElementById("input").value;
+    var n = 0;
+    var total = 0;
+    for (i = num.length - 1; i >= 0; i--){
+        total = total + parseInt(num[i]) * (2**n);
+        n = n + 1
+        console.log(total)
+    };
+   var finalTotal = document.getElementById("outputBox").append(total)
+
+}
