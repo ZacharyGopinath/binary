0df14e2e092939fd1993932ae5de6fff5ea59e6b
ZacharyGopinath <54950453+ZacharyGopinath@users.noreply.github.com>
2020-09-15 16:32:33 -0400

    Binary # Checking
    Added support for checking if the input is a binary number

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 function calculate(){
     var num = document.getElementById("input").value;
-    var n = 0;
-    var total = 0;
-    for (i = num.length - 1; i >= 0; i--){
-        total = total + parseInt(num[i]) * (2**n);
-        n = n + 1
-        console.log(total)
+    
+    for (i = num.length - 1l i >=0; i--){
+        if (num[i] != 1 or num[i] != 0){
+             var isValid = False;
+             }
     };
-   document.getElementById("outputBox").innerHTML = total;
-
+    if (isValid == True){
+        var n = 0;
+        var total = 0;
+        for (i = num.length - 1; i >= 0; i--){
+            total = total + parseInt(num[i]) * (2**n);
+            n = n + 1
+            console.log(total)
+        };
+       document.getElementById("outputBox").innerHTML = total;
+    }else{
+        document.getElementById("outputBox").innerHTML = "Not A Valid Binary #";
 }
